161b8fd5c94d29dec07e15a75abebc3290441a59
Christopher Kim <chris.kim1990@gmail.com>
2020-11-05 16:14:44 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,11 +3,63 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
-board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], # Top row
                    [0, 3, 6], [1, 4, 7], [2, 5, 8],   # Middle Row
                    [0, 4, 8], [2, 4, 6] ]             # Diagonals
+
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+
+top_row_win = [0, 1, 2]
+mid_row_win = [3, 4, 5]
+bot_row_win = [6, 7, 8]
+diag_row_win1 = [0, 4, 8]
+diag_row_win2 = [2, 4, 6]
+dleft_row_win = [0, 3, 6]
+dmid_row_win = [1, 4, 7]
+dright_row_win = [2, 5, 8]
+
+if board[top_row_win[0]] == "X" && board[top_row_win[1]] == "X" &&
+  board[top_row_win[2]] == "X"
+  "X won in the top row"
+end
+
+if board[mid_row_win[3]] == "X" && board[mid_row_win[4]] == "X" &&
+  board[mid_row_win[5]] == "X"
+  "X won in the middle row"
+end
+
+if board[bot_row_win[6]] == "X" && board[bot_row_win[7]] == "X" &&
+  board[bot_row_win[8]] == "X"
+  "X won in the bottom row"
+end
+
+if board[diag_row_win1[0]] == "X" && board[diag_row_win1[4]] == "X" &&
+  board[diag_row_win1[8]] == "X"
+  "X won diagonally"
+end
+
+if board[diag_row_win2[2]] == "X" && board[diag_row_win2[4]] == "X" &&
+  board[diag_row_win2[6]] == "X"
+  "X won diagonally"
+end
+
+if board[dleft_row_win1[0]] == "X" && board[dleft_row_win1[3]] == "X" &&
+  board[dleft_row_win1[6]] == "X"
+  "X won in the left row"
+end
+
+if board[dmid_row_win1[1]] == "X" && board[dmid_row_win1[4]] == "X" &&
+  board[dmid_row_win1[7]] == "X"
+  "X won in the middle row"
+end
+
+if board[dright_row_win1[2]] == "X" && board[dright_row_win1[5]] == "X" &&
+  board[dright_row_win1[8]] == "X"
+  "X won in the right row"
+end
+
 
 win_index_1 = WIN_COMBINATIONS[0]
 win_index_2 = WIN_COMBINATIONS[1]
@@ -31,7 +83,7 @@ position_8 = board[win_index_8]
 
 def won?(board)
 if position_1 == "X" && position_2 == "X" && position_3 == "X"
-  return win_combination[1][2][3]# return the win_combination indexes that won.
+  return win_combination[1, 2, 3]# return the win_combination indexes that won.
 else
   false
 end
