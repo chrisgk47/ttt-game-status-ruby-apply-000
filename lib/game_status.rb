b7ef7f582ab1d46342cf712c5dcf8cd89811860b
Christopher Kim <chris.kim1990@gmail.com>
2020-11-05 17:32:50 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -38,7 +38,7 @@ def draw?(board)
 end
 
 def over?(board)
-  if ((won?(board) == true) || (full?(board) == true) || (draw?(board) == true))
+  if ((won?(board) == true) || (draw?(board) == true) || (full?(board) == true))
     true
   end
     false
