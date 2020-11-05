2f10afae46557484be4b3c6343fcff38d4f7a8a6
Christopher Kim <chris.kim1990@gmail.com>
2020-11-05 15:56:32 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -3,6 +3,7 @@ def position_taken?(board, index)
   !(board[index].nil? || board[index] == " ")
 end
 
+board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 # Define your WIN_COMBINATIONS constant
 WIN_COMBINATIONS = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], # Top row
                    [0, 3, 6], [1, 4, 7], [2, 5, 8],   # Middle Row
