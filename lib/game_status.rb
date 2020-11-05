9b77674f000737eddb0c538fbcf4aa7d823b031f
Christopher Kim <chris.kim1990@gmail.com>
2020-11-05 15:53:04 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -4,3 +4,33 @@ def position_taken?(board, index)
 end
 
 # Define your WIN_COMBINATIONS constant
+WIN_COMBINATIONS = [ [0, 1, 2], [3, 4, 5], [6, 7, 8], # Top row
+                   [0, 3, 6], [1, 4, 7], [2, 5, 8],   # Middle Row
+                   [0, 4, 8], [2, 4, 6] ]             # Diagonals
+
+def won?(board)
+
+win_index_1 = WIN_COMBINATIONS[0]
+win_index_2 = WIN_COMBINATIONS[1]
+win_index_3 = WIN_COMBINATIONS[2]
+win_index_4 = WIN_COMBINATIONS[3]
+win_index_5 = WIN_COMBINATIONS[4]
+win_index_6 = WIN_COMBINATIONS[5]
+win_index_7 = WIN_COMBINATIONS[6]
+win_index_8 = WIN_COMBINATIONS[7]
+
+position_1 = board[win_index_1]
+position_2 = board[win_index_2]
+position_3 = board[win_index_3]
+position_4 = board[win_index_4]
+position_5 = board[win_index_5]
+position_6 = board[win_index_6]
+position_7 = board[win_index_7]
+position_8 = board[win_index_8]
+
+if position_1 == "X" && position_2 == "X" && position_3 == "X"
+  return win_combination # return the win_combination indexes that won.
+else
+  false
+end
+end
