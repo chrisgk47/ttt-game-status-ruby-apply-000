bfdb553f819f67e2fe7c100107799560613fc174
Christopher Kim <chris.kim1990@gmail.com>
2020-11-05 17:06:38 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,7 +16,7 @@ def won?(board)
   if board[win_combo[0] == "X" && board[win_combo[1]] == "X" && board[win_combo[2]] == "X" ||
      board[win_combo[0]] == "O" && board[win_combo[1]] == "O" && board[win_combo[2]] == "O"
      return win_combo
-    end
-end
-  false
+  else
+    false
+  end
 end
